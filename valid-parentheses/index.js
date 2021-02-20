8794d7f9b09056960e7781efdf157a439b7bd66c
Edney Pitta <edney.pitta@farfetch.com>
2021-02-20 21:20:56 +0000

    feat: add valid-parentheses solution

---
+++ valid-parentheses/index.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  const stack = [];
+  const openingChars = new Set(["(", "{", "["]);
+
+  for (const char of s) {
+    if (openingChars.has(char)) {
+      stack.push(char);
+      continue;
+    }
+
+    if (stack.pop() !== getOpeningChar(char)) return false;
+  }
+
+  return true;
+};
+
+var getOpeningChar = function (char) {
+  switch (char) {
+    case ")":
+      return "(";
+    case "}":
+      return "{";
+    case "]":
+      return "[";
+  }
+};
